8dab09844cc64312f69efb6c5aee810af882b223
hoemjuusto <juusokorhoin@gmail.com>
2019-10-30 12:08:10 +0200

    added delete empty lines

---
+++ build/CMakeFiles/assignment1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/juuso/CLionProjects/assignment/child-process.c" "/home/juuso/CLionProjects/assignment/build/CMakeFiles/assignment1.dir/child-process.c.o"
+  "/home/juuso/CLionProjects/assignment/file_edits.c" "/home/juuso/CLionProjects/assignment/build/CMakeFiles/assignment1.dir/file_edits.c.o"
+  "/home/juuso/CLionProjects/assignment/filehandlers.c" "/home/juuso/CLionProjects/assignment/build/CMakeFiles/assignment1.dir/filehandlers.c.o"
+  "/home/juuso/CLionProjects/assignment/main.c" "/home/juuso/CLionProjects/assignment/build/CMakeFiles/assignment1.dir/main.c.o"
+  "/home/juuso/CLionProjects/assignment/signalling.c" "/home/juuso/CLionProjects/assignment/build/CMakeFiles/assignment1.dir/signalling.c.o"
+  "/home/juuso/CLionProjects/assignment/testmain.c" "/home/juuso/CLionProjects/assignment/build/CMakeFiles/assignment1.dir/testmain.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment1.dir/child-process.c.o"
+  "CMakeFiles/assignment1.dir/file_edits.c.o"
+  "CMakeFiles/assignment1.dir/filehandlers.c.o"
+  "CMakeFiles/assignment1.dir/main.c.o"
+  "CMakeFiles/assignment1.dir/signalling.c.o"
+  "CMakeFiles/assignment1.dir/testmain.c.o"
+  "assignment1"
+  "assignment1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
